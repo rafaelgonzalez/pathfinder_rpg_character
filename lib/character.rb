918da9ae8963184b558b71e1f713b7cedae66356
Rafaël Gonzalez <rafaelgonzalez@users.noreply.github.com>
2014-02-24 13:28:26 +0100

    Rework Race extension to match Skills and Klasses behavior

--- lib/character.rb
+++ lib/character.rb
@@ -11,23 +11,11 @@ class Character
   attr_reader :armor_class, :saving_throws
 
   def initialize
-    extend Character::Race::Empty
+    extend Character::Races
     extend Character::Klasses
     extend Character::Skills
 
     @armor_class = ArmorClass.new(self)
     @saving_throws = SavingThrows.new(self)
-  end
-
-  def set_race(race_name)
-    race_class_name = race_name.to_s.camelize.to_sym
-
-    available_races = Race.list_available
-
-    unless available_races.include? race_class_name
-      raise ArgumentError.new("Wrong race name, choose one of the following: #{available_races}")
-    end
-
-    extend "Character::Race::#{race_class_name}".constantize
   end
 end
