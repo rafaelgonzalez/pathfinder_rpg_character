fae5e75825576bd3b9207274c1fa9bdd68023251
Rafaël Gonzalez <rafaelgonzalez@users.noreply.github.com>
2014-02-25 13:31:54 +0100

    Document Character::SavingThrows

--- lib/character/saving_throws.rb
+++ lib/character/saving_throws.rb
@@ -4,14 +4,17 @@ class Character
       @character = character
     end
 
+    # @return [Fixnum] the fortitude saving throw.
     def fortitude
       @character.constitution_modifier + @character.klasses.map(&:fortitude_save_base).inject(:+)
     end
 
+    # @return [Fixnum] the reflex saving throw.
     def reflex
       @character.dexterity_modifier + @character.klasses.map(&:reflex_save_base).inject(:+)
     end
 
+    # @return [Fixnum] the will saving throw.
     def will
       @character.wisdom_modifier + @character.klasses.map(&:will_save_base).inject(:+)
     end
