21b622e7e512c597092660b2af7a0f48edc096d8
Rafaël Gonzalez <rafaelgonzalez@users.noreply.github.com>
2014-10-16 23:50:08 +1100

    Remove Coveralls

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,5 @@
 require 'simplecov'
-require 'coveralls'
 
-SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
-  SimpleCov::Formatter::HTMLFormatter,
-  Coveralls::SimpleCov::Formatter
-]
 SimpleCov.start
 
 require 'pathfinder_rpg'
